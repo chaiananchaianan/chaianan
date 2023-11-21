419bf3e70226695393be302e290810775d271e7b
chaianan <omidomidomif7899@gmail.com>
2023-11-21 11:18:37 +0700

    2343542354

--- pages/sumbit.php
+++ pages/sumbit.php
@@ -2,24 +2,30 @@
     $email = $_POST['email'];
     $email = $_POST['password'];
     $email = $_POST['city']; 
-    $email = $_POST['wed'];
+    $email = $_POST['web'];
     $email = $_POST['role'];
-    $email = $_POST['sign1'];
-    $email = $_POST['sign2'];
-    $email = $_POST['sign3'];
-
     
-    echo 'email: '.$email.'<br>';
-    echo 'password: '.$password.'<br>';
-    echo 'city: '.$city.'<br>';
-    echo 'wed: '.$wed.'<br>';
-    echo 'role: '.$role.'<br>';
-    echo 'sign1: '.$sign1.'<br>';
-    echo 'sign2: '.$sign2.'<br>';
-    echo 'sign3: '.$sign3.'<br>';
 
+    $severname = 'localhost:8080';
+    $username = 'root';
+    $password = '123456';
+    $dbname = "db_employee";
 
+    conn = new mysqli($severname, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("".$conn->connect_error);
+    }
 
+    $sql = "INSERT INTO employee (username, password, city, webserver, role)
+            VALUES ($email,$password,$city,$web,$role)";
 
+    $result = $conn->query($sql);
 
-?>
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+    
+    }   else{
+        echo"error".$sql"<br>".$conn->error;
+    }
+    $conn->close();
+    ?>
